f0992c249b791c865aa2fdcbf43d9e1b2f545a66
oceanyatchregistration-ui <oceanyatchregistration@gmail.com>
2026-09-26 19:27:52 +0500

    Remove unnecessary tracking sequence migration

--- drizzle/0006_client_tracking_reference.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `reference_sequences` (
-  `name` text PRIMARY KEY NOT NULL,
-  `next_value` integer NOT NULL
-);
---> statement-breakpoint
-INSERT INTO `reference_sequences`(`name`,`next_value`) VALUES('customer_tracking',990);
